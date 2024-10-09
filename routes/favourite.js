d2a4014458be84328c581f9aff9b42a3f6b08255
BaoHoang2004 <nvbaohoang@gmail.com>
2024-10-09 13:29:30 +0700

    api favourite

--- routes/favourite.js
+++ routes/favourite.js
@@ -4,12 +4,11 @@ var router = express.Router();
 const favouriteController = require('../src/controller/FavouriteController');
 const { createResponse } = require('../src/helper/createResponse.helper');
 
-router.post('/api/addFourite', async function (req, res, next) {
+router.post('/api/add', async function (req, res, next) {
     const { userId, tourId } = req.body
     try {
         const data = await favouriteController.insert({ userId: userId, tourId: tourId })
         if (data) {
-            res.json({ code: 1, message: "", })
             return res.json(createResponse(200, "Thêm vào mục yêu thích thành công", "success", data));
         } else {
             return res.json(createResponse(500, "Thêm vào mục yêu thích thất bại", "error"));
@@ -19,15 +18,53 @@ router.post('/api/addFourite', async function (req, res, next) {
         return res.json(createResponse(500, "Lỗi máy chủ khi thêm vào mục yêu thích.", "error"));
     }
 });
-router.post('/api/getFavourite', async function (req, res) {
+
+
+router.post('/api/favourite', async function (req, res) {
+    const { userId, tourId } = req.body
+    try {
+        if (!userId || !tourId) {
+            return res.json(createResponse(400, "Không đủ dữ liệu.", "false"));
+        }
+        const data = await favouriteController.favourite(userId, tourId)
+        if (data == 1) {
+            return res.json(createResponse(200, "Thêm thành công", "success"));
+        } else if (data == 0) {
+            return res.json(createResponse(200, "xóa thành công", "success"));
+        }
+        return res.json(createResponse(400, "Lỗi favourite", "false"));
+    } catch (error) {
+        console.log("===== Lỗi get Favourite =====", error);
+        return res.json(createResponse(500, "Lỗi máy chủ khi lấy danh sách yêu thích.", "error"));
+    }
+})
+
+router.post('/api/deleteFavourite', async function (req, res) {
+    const { tourId } = req.body
+    try {
+        const data = await favouriteController.remove(tourId)
+        if (data) {
+            return res.json(createResponse(200, "Xóa yêu thích thành công", "success", data));
+        } else {
+            return res.json(createResponse(404, "Lỗi xóa.", "error"));
+        }
+    } catch (error) {
+        console.log("===== Lỗi get Favourite =====", error);
+        return res.json(createResponse(500, "Lỗi máy chủ khi lấy danh sách yêu thích.", "error"));
+    }
+})
+
+router.post('/api/getFavouriteByUser', async function (req, res) {
     const { userId } = req.body
     try {
-        const data = await favouriteController.getAll(userId)
+        if (!userId) {
+            return res.json(createResponse(400, "Không đủ dữ liệu.", "false"));
+        }
+        const data = await favouriteController.getByUser(userId)
         if (data) {
             return res.json(createResponse(200, "Lấy danh sách yêu thích thành công", "success", data));
-        } else {
-            return res.json(createResponse(404, "Không có mục yêu thích nào.", "error"));
         }
+        return res.json(createResponse(400, "Lỗi favourite", "false"));
     } catch (error) {
         console.log("===== Lỗi get Favourite =====", error);
         return res.json(createResponse(500, "Lỗi máy chủ khi lấy danh sách yêu thích.", "error"));
--- src/controller/FavouriteController.js
+++ src/controller/FavouriteController.js
@@ -1,5 +1,42 @@
 const mongoose = require('mongoose');
 const _Favourite = require('../modules/FavouriteModule')
+const _Tour = require('../modules/TourModule')
+const _User = require('../modules/UserModle')
+
+
+
+const favourite = async (userId, tourId) => {
+    try {
+        let a = false
+        const tour = await _Favourite.find({ userId: userId })
+        tour.forEach((value, key) => {
+            if (value.tourId == tourId) {
+                a = true
+                return -1
+            }
+        });
+        if (a) {
+            await _Favourite.deleteOne({ tourId: tourId, userId: userId })
+            return 0; // 0- xóa thành công
+        } else {
+            const data = new _Favourite({ userId: userId, tourId: tourId })
+            await data.save()
+            return 1;
+        }
+
+    } catch (error) {
+        console.log("=====Lỗi Favourite=====", error);
+        return false
+    }
+}
+
+// favourite("670603408e6c85a630295708", "6704a28526be2256863506e1")
+
+
+
+
+
+
 
 const insert = async (userId, tourId) => {
     try {
@@ -27,4 +64,107 @@ const getAll = async (userId) => {
     }
 }
 
-module.exports = { insert, getAll }
+const remove = async (tourId) => {
+    try {
+        await _Favourite.deleteOne({ tourId: tourId });
+        return 0;
+    }
+    catch (error) {
+        console.log(error);
+        return false
+    }
+}
+
+const getByUser = async (userId) => {
+    try {
+        const tours = await _Favourite.aggregate([
+            {
+                $match: {
+                    userId: new mongoose.Types.ObjectId(userId),
+                }
+            },
+            {
+                $lookup: {
+                    from: 'tours', // Collection Image
+                    localField: 'tourId',
+                    foreignField: '_id',
+                    as: 'tour'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'images', // Collection Image
+                    localField: 'tourId',
+                    foreignField: 'tourId',
+                    as: 'images'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'locations',
+                    localField: 'tourId',
+                    foreignField: 'tourId',
+                    as: 'locations'
+                }
+            },
+            {
+                $lookup: {
+                    from: 'details',
+                    localField: 'tourId',
+                    foreignField: 'tourId',
+                    as: 'details'
+                }
+            },
+            {
+                // Lọc các detail có status là 1
+                $addFields: {
+                    details: {
+                        $filter: {
+                            input: "$details",
+                            as: "detail",
+                            cond: { $eq: ["$$detail.status", "1"] } // Điều kiện: status = 1
+                        }
+                    }
+                }
+            },
+            {
+                $project: {
+                    _id: 1,
+                    tour: {
+                        tourName: 1,
+                        description: 1,
+                        status: 1,
+                        createAt: 1,
+                    },
+                    locations: {
+                        departure: 1,
+                        destination: 1
+                    },
+                    details: {
+                        priceAdult: 1,
+                        startDay: 1,
+                        endDay: 1,
+                        maxTicket: 1,
+                        minTicket: 1,
+                        priceAdult: 1,
+                        priceChildren: 1,
+                        PromotionalPrice: 1
+                    },
+                    images: { linkImage: 1 }
+                }
+            }
+
+        ]);
+
+        if (!tours.length) {
+            return false;
+        }
+
+        return tours;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+}
+
+module.exports = { insert, getAll, getByUser, remove, favourite }
