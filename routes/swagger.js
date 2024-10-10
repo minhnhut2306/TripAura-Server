f22dbe0c4da90b62afb065aa6c6895bd0576f0b1
minhnhut2306 <129873898+minhnhut2306@users.noreply.github.com>
2024-10-10 08:45:48 +0700

    sửa sổ tự động

--- routes/swagger.js
+++ routes/swagger.js
@@ -10,60 +10,21 @@ const options = {
             description: 'API Documentation for your Node.js project',
         },
         tags: [
-            {
-                name: "User",
-                description: "User-related operations"
-            },
-            {
-                name: "Booking",
-                description: "Booking-related operations"
-            },
-            {
-                name: "Category",
-                description: "Category-related operations"
-            },
-            {
-                name: "Detail",
-                description: "Detail-related operations"
-            },
-            {
-                name: "Favourite",
-                description: "Favourite-related operations"
-            },
-            {
-                name: "Image",
-                description: "Image-related operations"
-            },
-            {
-                name: "Location",
-                description: "Location-related operations"
-            },
-            {
-                name: "Review",
-                description: "Review-related operations"
-            },
-            {
-                name: "Tour",
-                description: "Tour-related operations"
-            },
-            {
-                name: "Voucher",
-                description: "Voucher-related operations"
-            }, 
-            {
-                name: "VoucherType",
-                description: "VoucherType-related operations"
-            }
+            { name: "User", description: "User-related operations" },
+            { name: "Booking", description: "Booking-related operations" },
+            { name: "Category", description: "Category-related operations" },
+            { name: "Detail", description: "Detail-related operations" },
+            { name: "Favourite", description: "Favourite-related operations" },
+            { name: "Image", description: "Image-related operations" },
+            { name: "Location", description: "Location-related operations" },
+            { name: "Review", description: "Review-related operations" },
+            { name: "Tour", description: "Tour-related operations" },
+            { name: "Voucher", description: "Voucher-related operations" },
+            { name: "VoucherType", description: "VoucherType-related operations" }
         ],
         servers: [
-            {
-                url: 'https://trip-aura-server.vercel.app/',
-                description: 'Production server',
-            },
-            {
-                url: 'https://trip-aura-server-git-main-minhnhut2306s-projects.vercel.app/',
-                description: 'Production server',
-            },
+            { url: 'https://trip-aura-server.vercel.app/', description: 'Production server' },
+            { url: 'https://trip-aura-server-git-main-minhnhut2306s-projects.vercel.app/', description: 'Production server' }
         ],
     },
     apis: ['./routes/*.js'],
@@ -72,5 +33,10 @@ const options = {
 const swaggerSpec = swaggerJsdoc(options);
 
 module.exports = (app) => {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+        swaggerOptions: {
+            docExpansion: 'none',
+            defaultModelsExpandDepth: -1  
+        }
+    }));
 };
